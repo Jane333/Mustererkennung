a33bbf1ee175bc735158005555f318104239d297
Jane333 <jana.cavojska9@gmail.com>
2015-05-04 00:26:18 +0200

    korrigierte Aufg. 1 in skript.m uebernommen

--- ueb02/skript.m
+++ ueb02/skript.m
@@ -8,28 +8,26 @@ coder.extrinsic('disp')
 disp('Aufgabe 1')
 
 % Spalte 1 = Huhn-ID, Spalte 2 = Gewicht, Spalte 3 = Futterklasse
+
 A = load('chickwts_training.csv');
 A_Sorted = sortrows(A,2); % nach Gewichten sortieren
 A_Training = A(:,2:3);
 U = unique(A_Training,'rows');
 zeilen = size(U,1);
-
-% Spalte 1 = Huhn-ID, Spalte 2 = Gewicht, Spalte 3 = Futterklasse
 B = load('chickwts_testing.csv');
 B_Testing = B(:,2:3);
 
-for k = [1,3,5]
-
-  dists = zeros(2*k - 1, 2);
-  % (2*k-1)x2-Matrix mit den 5 Kandidat-Nachbarn um unser Eingabehuhn h herum.
-  % 3 von diesen 5 Nachbarn sind die 3 nearest neighbors unseres Huhns.
-  % 1. Spalte enthaelt die Distanzen, 2. Spalte enthaelt die Klassen dieser Nachbarn.
-  dists = dists - 1; % weil ich eine -1 als Initialwert schoener finde als ne 0
-		
+alle_k = [1 3 5];
+for k=alle_k
+  fprintf('k = %i\n',k)
+  dists = zeros(2*k - 1, 2); %  (2*k-1)x2-Matrix mit den 5 Kandidat-Nachbarn 
+  % um unser Eingabehuhn h herum. 3 von diesen 5 Nachbarn sind die 3 nearest 
+  % neighbors unseres Huhns. 1. Spalte enthaelt die Distanzen, 2. Spalte enthaelt
+  % die Klassen dieser Nachbarn.
+  
   C = []; % Ergebnismatrix
- 
-  for zeilenindex_testdaten = 1:size(A_Training,1)
-    h = A_Training(zeilenindex_testdaten, 1); % unser Testhuhn
+  for zeilenindex_testdaten = 1:size(B_Testing,1)
+    h = B_Testing(zeilenindex_testdaten, 1); % unser Testhuhn
     % Huhn 'treffer' finden, das unserem Huhn h am naechsten ist:
     treffer = -1;
     trefferZeile = -1; % Zeile, in welcher wir das 'treffer'-Huhn gefunden haben
@@ -40,6 +38,7 @@ for k = [1,3,5]
       treffer = U(zeilen,2);
       trefferZeile = zeilen;
     else
+    
       for z = 1:zeilen
         if h == U(z, 1)
           treffer = U(z, 2);
@@ -58,9 +57,9 @@ for k = [1,3,5]
             break
           end
         end
-      end	
+      end  
     end
-    
+    % Wir haben unseren 'treffer' gefunden. Nun entscheiden wir, welche Huehner um ihn herum unsere k naechsten Nachbarn sind.
     if k == 1
       most_frequent_neighbor_class = treffer;
     elseif k > 1
@@ -70,22 +69,19 @@ for k = [1,3,5]
       elseif trefferZeile > zeilen - k + 1
         offset = zeilen - k + 1 - trefferZeile; % 10-3+1-10 = -2
       end
-      % aus U ein Fenster der Laenge 2*k-1 um 'treffer' herum ausschneiden
-      dists = U(trefferZeile-(k-1)+offset:trefferZeile+(k-1)+offset, :);
-      % Gewichte ersetzen durch Distanzen der Gewichte zu h.
-      dists = [abs((dists(:,1)) - h), dists(:,2)]; 
-      % nach Gewicht-Distanzen sortieren
-      dists = sortrows(dists,1);
-      % die Klassen der k Nachbarn mit den kleinsten Gewicht-Distanzen holen
-      k_neighbors_classes = dists(1:k, 2);
-      % findet die haeufigste Klasse in k_neighbors_classes
-      most_frequent_neighbor_class = mode(k_neighbors_classes);
+      dists = U(trefferZeile-(k-1)+offset:trefferZeile+(k-1)+offset, :); % aus U ein Fenster der Laenge 2*k-1 um 'treffer' herum ausschneiden
+      dists = [abs((dists(:,1)) - h), dists(:,2)]; % Gewichte ersetzen durch Distanzen der Gewichte zu h.
+      dists = sortrows(dists,1); % nach Gewicht-Distanzen sortieren
+      k_neighbors_classes = dists(1:k, 2); % die Klassen der k Nachbarn mit den kleinsten Gewicht-Distanzen holen
+      most_frequent_neighbor_class = mode(k_neighbors_classes); % findet die haeufigste Klasse in k_neighbors_classes
     end
+    
     % Ergebnismatrix C: 1. Spalte: Gewicht, 2. Spalte: Futterklasse (Testdaten), 3. Spalte: Futterklasse (Trainingsdaten)
-    tmpVector = [A_Training(zeilenindex_testdaten, 1), A_Training(zeilenindex_testdaten, 2), most_frequent_neighbor_class];
-    C = vertcat(C,tmpVector);
+    tmpVector = [B_Testing(zeilenindex_testdaten, 1), B_Testing(zeilenindex_testdaten, 2), most_frequent_neighbor_class];
+      C = vertcat(C,tmpVector);
+      
   end % end of for each test chicken
-		
+  
   % Konfusionsmatrix
   knownClass = C(:, 2);
   predictedClass = C(:, 3);
@@ -99,9 +95,10 @@ for k = [1,3,5]
       korrekt_vorhergesagt = korrekt_vorhergesagt + 1;
     end
   end
-
   Klassifikationsguete = korrekt_vorhergesagt / alle
 end % end of for k in 1, 3, 5
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
