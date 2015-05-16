e8b987a5ce84c133f39cba52fe3f89e28cd64311
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-05-16 19:40:00 +0200

    skript03.m added

---
+++ ueb03/skript03.m
@@ -0,0 +1,9 @@
+% Uebung 03
+% ----------
+% Video zu PCA: https://www.youtube.com/watch?v=7OAs0h0kYmk
+%
+% J. Cavojska, N. Lehmann
+
+% Trainingsdaten und Testdaten laden
+A = load('pendigits-training.txt');
+B = load('pendigits-testing.txt');
