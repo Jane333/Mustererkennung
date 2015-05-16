6c7c7c5f718dd3df9d68d2daff5446614cdb2cda
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-05-16 19:56:37 +0200

    clusters.txt added

--- ueb03/skript03.m
+++ ueb03/skript03.m
@@ -7,3 +7,31 @@
 % Trainingsdaten und Testdaten laden
 A = load('pendigits-training.txt');
 B = load('pendigits-testing.txt');
+
+% Aufgabe 1 (3 Punkte)
+
+% A 1.1: multivariate (mehrdimensionale) Normalverteilung
+%        (Erwartungswert, Kovarianzmatrix) berechnen
+
+% A 1.2: Testdaten anhand der A-Posteriori-PDF klassifizieren,
+%        Konfusionsmatrix und Klassifikationsgüte angeben
+%        (Annahme: Gleichverteilte A-Priori-Wahrscheinlichkeit
+%                  für jede Ziffer)
+
+% Aufgabe 2 (4 Punkte)
+
+% A 2.1: Erste Hauptkomponente der Trainingsdaten angeben
+
+% A 2.2: Dimensionsreduzierung mittels PCA,
+%        Testdaten klassifizieren mit Bayes Klassifikator
+%        (wie in Aufgabe 1)
+%        Klassifikationsgüte für alle Dimensionen angeben
+
+% Aufgabe 3 (3 Punkte)
+
+% A 3.1: k-means auf die Daten clusters.txt anwenden,
+%        k-means soll selbst implementiert werden!
+
+% A 3.2: Clusterzentren und Zuordnungen der Punkte
+%        der ersten 5 Iterationsschritte mit k=3
+%        visualisieren (insgesamt 5 Bilder)
