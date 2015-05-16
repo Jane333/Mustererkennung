88ea27fc0e26b02d7c8a2e91d134180296ade7fb
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-05-16 23:53:57 +0200

    skript3.m adjusted

--- ueb03/skript03.m
+++ ueb03/skript03.m
@@ -25,13 +25,15 @@ A_8 = A((A(:,17)==8),:);
 A_9 = A((A(:,17)==9),:);
 X = A(:,1:A_n -1);
 x = min(X):max(X);
+B_n   = size(B,2);
+B_m   = size(B,1);
 
 % Aufgabe 1 (3 Punkte)
 
 % A 1.1: multivariate (mehrdimensionale) Normalverteilung
 %        (Erwartungswert, Kovarianzmatrix) berechnen
 
-% Erwartungswert für jede Koordinate für jeden Zug (0 bis 9)
+% Erwartungswert fuer jede Koordinate fuer jeden Zug (0 bis 9)
 E_A_0 = mean(A_0(:,1:A_n -1));
 E_A_1 = mean(A_1(:,1:A_n -1));
 E_A_2 = mean(A_2(:,1:A_n -1));
@@ -43,7 +45,7 @@ E_A_7 = mean(A_7(:,1:A_n -1));
 E_A_8 = mean(A_8(:,1:A_n -1));
 E_A_9 = mean(A_9(:,1:A_n -1));
 
-% Kovarianzmatrix für jeden Zug (0 bis 9)
+% Kovarianzmatrix fuer jeden Zug (0 bis 9)
 CVM_A_0 = cov(A_0(:,1:A_n -1));
 CVM_A_1 = cov(A_1(:,1:A_n -1));
 CVM_A_2 = cov(A_2(:,1:A_n -1));
@@ -55,7 +57,7 @@ CVM_A_7 = cov(A_7(:,1:A_n -1));
 CVM_A_8 = cov(A_8(:,1:A_n -1));
 CVM_A_9 = cov(A_9(:,1:A_n -1));
 
-% Multivariante PDF generieren für jeden Zug (0 bis 9)
+% Multivariante PDF generieren fuer jeden Zug (0 bis 9)
 A_0_mvpdf = mvnpdf(A_0(:,1:A_n -1), E_A_0, CVM_A_0);
 A_1_mvpdf = mvnpdf(A_1(:,1:A_n -1), E_A_1, CVM_A_1);
 A_2_mvpdf = mvnpdf(A_2(:,1:A_n -1), E_A_2, CVM_A_2);
@@ -67,10 +69,16 @@ A_7_mvpdf = mvnpdf(A_7(:,1:A_n -1), E_A_7, CVM_A_7);
 A_8_mvpdf = mvnpdf(A_8(:,1:A_n -1), E_A_8, CVM_A_8);
 A_9_mvpdf = mvnpdf(A_9(:,1:A_n -1), E_A_9, CVM_A_9);
 
-% A-Priori-Wahrscheinlichkeit für jeden Zug (0 bis 9)
+
+% A 1.2: Testdaten anhand der A-Posteriori-PDF klassifizieren,
+%        Konfusionsmatrix und Klassifikationsguete angeben
+%        (Annahme: Gleichverteilte A-Priori-Wahrscheinlichkeit
+%                  fuer jede Ziffer)
+
+% A-Priori-Wahrscheinlichkeit fuer jeden Zug (0 bis 9)
 A_x_apriori = 1 / length(unique(A(:,A_n)));
 
-% A-Posteriori-Wahrscheinlichkeit für jeden Zug (0 bis 9)
+% A-Posteriori-Wahrscheinlichkeit fuer jeden Zug (0 bis 9)
 A_0_aposteriori = A_0_mvpdf * A_x_apriori;
 A_1_aposteriori = A_1_mvpdf * A_x_apriori;
 A_2_aposteriori = A_2_mvpdf * A_x_apriori;
@@ -82,21 +90,35 @@ A_7_aposteriori = A_7_mvpdf * A_x_apriori;
 A_8_aposteriori = A_8_mvpdf * A_x_apriori;
 A_9_aposteriori = A_9_mvpdf * A_x_apriori;
 
-% A 1.2: Testdaten anhand der A-Posteriori-PDF klassifizieren,
-%        Konfusionsmatrix und Klassifikationsguete angeben
-%        (Annahme: Gleichverteilte A-Priori-Wahrscheinlichkeit
-%                  fuer jede Ziffer)
+% Und wie klassifiziert man jetzt einen 1x16 dimensionalen Vektor?
+%cp = classperf(B,c)
+% Konfusionsmatrix  
+%  confusionmat(knownClass, predictedClass)
+
+% Klassifikationsguete
+%  alle = size(D, 1);
+%  korrekt_vorhergesagt = 0;
+%  for z = 1:alle
+%    if D(z, 2) == D(z, 3)
+%      korrekt_vorhergesagt = korrekt_vorhergesagt + 1;
+%    end
+%  end
+%  Klassifikationsguete = korrekt_vorhergesagt / alle
 
 % Aufgabe 2 (4 Punkte)
 
 % A 2.1: Erste Hauptkomponente der Trainingsdaten angeben
+M_pca = princomp(zscore(B(:,1:B_n -1))); % pca with standardized variables
+M_pca_fst = M_pca(:,1); % ?
 
 % A 2.2: Dimensionsreduzierung mittels PCA,
 %        Testdaten klassifizieren mit Bayes Klassifikator
 %        (wie in Aufgabe 1)
-%        KlassifikationsgÃ¼te fÃ¼r alle Dimensionen angeben
+%        Klassifikationsguete fuer alle Dimensionen angeben
 
 % Aufgabe 3 (3 Punkte)
+
+% Link: http://www-home.fh-konstanz.de/~mfranz/muk_files/kmeans.m
 
 % A 3.1: k-means auf die Daten clusters.txt anwenden,
 %        k-means soll selbst implementiert werden!
