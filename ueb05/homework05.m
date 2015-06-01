216eea5adb35ab492dbf2d83ef89a986f0c83ff4
Jane333 <jana.cavojska9@gmail.com>
2015-06-01 12:49:46 +0200

    matlab funktion uebernommen

--- ueb05/homework05.m
+++ ueb05/homework05.m
@@ -6,11 +6,15 @@ clc
 %%%%%%%%%%%%%%%%%  Datenaufbereitung  %%%%%%%%%%%%%%%%
 
 A = load('fisher.txt');
-Koordinaten = A(:,1:2)
-Klassen = A(:,3)
+Koordinaten = A(:,1:2);
+Klassen = A(:,3);
 
 %%%%%%%%%%%%%%%%%%%%%  Aufgabe 2  %%%%%%%%%%%%%%%%%%%%
 
 % Create a default (linear) discriminant analysis classifier:
-linclass = fitcdiscr(Koordinaten, Klassen);
+linclass = fitcdiscr(Koordinaten, Klassen)
 
+% Classify:
+meanmeas = mean(Koordinaten)
+meanclass = predict(linclass,meanmeas)
+meanclass = 'versicolor'
