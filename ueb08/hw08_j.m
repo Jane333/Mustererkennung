f059aba840648024173cea3cae6015fba1e1a6d0
Jane333 <jana.cavojska9@gmail.com>
2015-06-24 14:33:10 +0200

    Aufg. 1 a und 1 b fertig in hw08_j.m

--- ueb08/hw08_j.m
+++ ueb08/hw08_j.m
@@ -7,8 +7,39 @@ clc
 
 % Aufgabe 1a
 
-% Gewichtsvektor:
-v = [0.5 0.5 -0.8]  % AND
+v1 = [0.3; 0.4; 0.5]
+v2 = [-0.2; 0.8; 0.3]
+v3 = [1; 1; 0]
+%  v4 = [0.5; 0.5; -0.8]  % AND
 
-yf1 = f1(v)
-yf8 = f8(v)
+classify_vec(v1)
+classify_vec(v2)
+classify_vec(v3)
+
+
+% Aufgabe 1b)
+figure
+hold on
+[X,Y,Z] = sphere(100);
+x = [X(:); X(:); X(:)];
+y = [Y(:); Y(:); Y(:)];
+z = [Z(:); Z(:); Z(:)];
+scatter3(x,y,z, '.', 'm')
+scatter3(0,0,0, 'o', 'k', 'filled')
+title('Aufgabe 1b - DIE Kugel');
+xlabel('X Koordinate');
+ylabel('Y Koordinate');
+zlabel('Z Koordinate');
+axis([-1.1 1.1 -1.1 1.1]);
+legend('fancy pinky points');
+surf(0.99*X,0.99*Y,0.99*Z)
+
+% Vektoren
+Ortsvektoren = [x,y,z]';
+
+
+% Aufgabe 1c)
+
+
+% Aufgabe 1d)
+
