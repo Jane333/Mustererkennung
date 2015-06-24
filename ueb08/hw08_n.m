890dc9b4d6a190b2b5c69decbc2a16bc97d9704e
Nicolas Lehmann <mail@nicolaslehmann.de>
2015-06-24 13:57:21 +0200

    fancy pinky point sphere

--- ueb08/hw08_n.m
+++ ueb08/hw08_n.m
@@ -5,3 +5,33 @@ clc
 
 %%% Aufgabe 1 - Repräsentierbarkeit Boolescher Funktionen durch ein Perzeptron %%%
 
+b = [ 0,0,1 ; 0,1,1 ; 1,0,1 ; 1,1,1 ];
+
+% Aufgabe 1a)
+
+
+% Aufgabe 1b)
+figure
+hold on
+[X,Y,Z] = sphere(100);
+x = [X(:); X(:); X(:)];
+y = [Y(:); Y(:); Y(:)];
+z = [Z(:); Z(:); Z(:)];
+scatter3(x,y,z, '.', 'm')
+scatter3(0,0,0, 'o', 'k', 'filled')
+title('Aufgabe 1b - DIE Kugel');
+xlabel('X Koordinate');
+ylabel('Y Koordinate');
+zlabel('Z Koordinate');
+axis([-1.1 1.1 -1.1 1.1]);
+legend('fancy pinky points');
+surf(0.99*X,0.99*Y,0.99*Z)
+
+% Vektoren
+Ortsvektoren = [x,y,z]';
+
+% Aufgabe 1c)
+
+
+% Aufgabe 1d)
+
