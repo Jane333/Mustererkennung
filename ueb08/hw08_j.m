bf820117276eb9ca2ec00bddeef97150d6ffb22f
Jane333 <jana.cavojska9@gmail.com>
2015-06-24 13:57:11 +0200

    added the fx.m files

---
+++ ueb08/hw08_j.m
@@ -0,0 +1,14 @@
+% Clean up
+clear all
+close all
+clc
+
+%%% Aufgabe 1 - Repraesentierbarkeit Boolescher Funktionen durch ein Perzeptron %%%
+
+% Aufgabe 1a
+
+% Gewichtsvektor:
+v = [0.5 0.5 -0.8]  % AND
+
+yf1 = f1(v)
+yf8 = f8(v)
