2b62bf51aefe8e78b07f368c420406e7d829e5eb
Jane333 <jana.cavojska9@gmail.com>
2015-06-24 15:57:23 +0200

    added classify_vec.m

---
+++ ueb08/classify_vec.m
@@ -0,0 +1,37 @@
+function [y] = classify_vec(v)
+
+if f0(v)
+    y = 0;
+elseif f1(v)
+    y = 1;
+elseif f2(v)
+    y = 2;
+elseif f3(v)
+    y = 3;
+elseif f4(v)
+    y = 4;
+elseif f5(v)
+    y = 5;
+elseif f6(v)
+    y = 6;
+elseif f7(v)
+    y = 7;
+elseif f8(v)
+    y = 8;
+elseif f9(v)
+    y = 9;
+elseif f10(v)
+    y = 10;
+elseif f11(v)
+    y = 11;
+elseif f12(v)
+    y = 12;
+elseif f13(v)
+    y = 13;
+elseif f14(v)
+    y = 14;
+elseif f15(v)
+    y = 15;
+else
+    y = 666;
+end
