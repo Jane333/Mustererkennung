12c933a3dc58df3a76b1ad8ceffb006f9922ec5c
Jane333 <jana.cavojska9@gmail.com>
2015-06-24 15:56:11 +0200

    histogramm fast fertig

--- ueb08/hw08_j.m
+++ ueb08/hw08_j.m
@@ -7,10 +7,10 @@ clc
 
 % Aufgabe 1a
 
-v1 = [0.3; 0.4; 0.5]
-v2 = [-0.2; 0.8; 0.3]
-v3 = [1; 1; 0]
-%  v4 = [0.5; 0.5; -0.8]  % AND
+v1 = [0.3; 0.5; -0.4]   % f10
+v2 = [-0.8; -0.6; 0.5]  % f1
+v3 = [0.7; 0.6; -1]     % f8
+
 
 classify_vec(v1)
 classify_vec(v2)
@@ -35,8 +35,25 @@ legend('fancy pinky points');
 surf(0.99*X,0.99*Y,0.99*Z)
 
 % Vektoren
-Ortsvektoren = [x,y,z]';
+Ortsvektoren = [x,y,z]; % jede Zeile enthaelt ein x, y, z Tupel
 
+randomIndices = randi([1,30603], 1, 10000);
+while length(unique(randomIndices)) < 10000
+    r = randi([1,30603], 1, 1);
+    randomIndices = horzcat(randomIndices, r);
+end
+randomIndices = unique(randomIndices);
+
+choice = Ortsvektoren(randomIndices, :); % choose 10000 random Ortsvektoren from the 30603 Ortsvektoren we generated in 1b)
+
+classifications = [];
+for i = 1:10000
+    v = choice(i,:);
+    classifications = vertcat(classifications, classify_vec(v));
+end
+myhistogram = hist(classifications);
+
+tabulate(classifications) % creates a frequency table of data in vector classifications
 
 % Aufgabe 1c)
 
