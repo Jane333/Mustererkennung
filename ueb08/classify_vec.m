8c0413b8d2aa22f841e15895fb7948c05b27dad8
Jane333 <jana.cavojska9@gmail.com>
2015-06-25 12:36:29 +0200

    ueb08 fertig in hw08_j.m, warte auf Nicos OK

--- ueb08/classify_vec.m
+++ ueb08/classify_vec.m
@@ -1,36 +1,36 @@
-function [y] = classify_vec(v)
+function [y] = classify_vec(v, x)
 
-if f0(v)
+if f0(v, x)
     y = 0;
-elseif f1(v)
+elseif f1(v, x)
     y = 1;
-elseif f2(v)
+elseif f2(v, x)
     y = 2;
-elseif f3(v)
+elseif f3(v, x)
     y = 3;
-elseif f4(v)
+elseif f4(v, x)
     y = 4;
-elseif f5(v)
+elseif f5(v, x)
     y = 5;
-elseif f6(v)
+elseif f6(v, x)
     y = 6;
-elseif f7(v)
+elseif f7(v, x)
     y = 7;
-elseif f8(v)
+elseif f8(v, x)
     y = 8;
-elseif f9(v)
+elseif f9(v, x)
     y = 9;
-elseif f10(v)
+elseif f10(v, x)
     y = 10;
-elseif f11(v)
+elseif f11(v, x)
     y = 11;
-elseif f12(v)
+elseif f12(v, x)
     y = 12;
-elseif f13(v)
+elseif f13(v, x)
     y = 13;
-elseif f14(v)
+elseif f14(v, x)
     y = 14;
-elseif f15(v)
+elseif f15(v, x)
     y = 15;
 else
     y = 666;
--- ueb08/hw08_j.m
+++ ueb08/hw08_j.m
@@ -7,14 +7,13 @@ clc
 
 % Aufgabe 1a
 
-v1 = [0.3; 0.5; -0.4]   % f10
-v2 = [-0.8; -0.6; 0.5]  % f1
-v3 = [0.7; 0.6; -1]     % f8
+v1 = [0.3; 0.5; -0.4];   % f10
+v2 = [-0.8; -0.6; 0.5];  % f1
+v3 = [0.7; 0.6; -1];     % f8
 
-
-classify_vec(v1)
-classify_vec(v2)
-classify_vec(v3)
+classify_vec(v1, 0) % classify v1 using 0, 1 as weights
+classify_vec(v2, 0)
+classify_vec(v3, 0)
 
 
 % Aufgabe 1b)
@@ -24,7 +23,7 @@ hold on
 x = [X(:); X(:); X(:)];
 y = [Y(:); Y(:); Y(:)];
 z = [Z(:); Z(:); Z(:)];
-scatter3(x,y,z, '.', 'm')
+%  scatter3(x,y,z, '.', 'm')
 scatter3(0,0,0, 'o', 'k', 'filled')
 title('Aufgabe 1b - DIE Kugel');
 xlabel('X Koordinate');
@@ -34,9 +33,9 @@ axis([-1.1 1.1 -1.1 1.1]);
 legend('fancy pinky points');
 surf(0.99*X,0.99*Y,0.99*Z)
 
+% now randomly choose 10000 points out of the ca. 30000 points that currently make up the sphere surface:
 % Vektoren
 Ortsvektoren = [x,y,z]; % jede Zeile enthaelt ein x, y, z Tupel
-
 randomIndices = randi([1,30603], 1, 10000);
 while length(unique(randomIndices)) < 10000
     r = randi([1,30603], 1, 1);
@@ -45,18 +44,69 @@ end
 randomIndices = unique(randomIndices);
 
 choice = Ortsvektoren(randomIndices, :); % choose 10000 random Ortsvektoren from the 30603 Ortsvektoren we generated in 1b)
+scatter3(choice(:,1), choice(:,2), choice(:,3), '.', 'm'); % plot the 10000 randomly chosen points
 
+
+% Aufgabe 1c)
+
+% classify all of the 10000 randomly chosen Ortsvektoren:
 classifications = [];
 for i = 1:10000
     v = choice(i,:);
-    classifications = vertcat(classifications, classify_vec(v));
+    classifications = vertcat(classifications, classify_vec(v, 0)); % classify v using 0, 1 as weights
 end
-myhistogram = hist(classifications);
+figure('NumberTitle','off','Name','Histogram of Boolean Function Frequencies');
+histogram(classifications) % hier entsteht ein plot
 
 tabulate(classifications) % creates a frequency table of data in vector classifications
-
-% Aufgabe 1c)
+%  Value    Count   Percent
+%    0     3490     34.90%
+%    1      268      2.68%
+%    2      247      2.47%
+%    3      391      3.91%
+%    4      243      2.43%
+%    5      328      3.28%
+%    7      128      1.28%
+%    8      133      1.33%
+%   10      312      3.12%
+%   11      234      2.34%
+%   12      341      3.41%
+%   13      219      2.19%
+%   14      260      2.60%
+%   15     3406     34.06%
+frequencies = tabulate(classifications);
+maxfreq = max(frequencies(:,2))  % 3490
+minfreq = min(frequencies(:,2))  % 128
+verhaeltnis = maxfreq / minfreq  % 27.2656
 
 
 % Aufgabe 1d)
 
+classifications2 = [];
+for i = 1:10000
+    v = choice(i,:);
+    classifications2 = vertcat(classifications2, classify_vec(v, -1)); % classify v using -1, 1 as weights
+end
+figure('NumberTitle','off','Name','Histogram of -1, 1 Boolean Frequencies');
+histogram(classifications2) % hier entsteht ein plot
+
+tabulate(classifications2) % creates a frequency table of data in vector classifications2
+%  Value    Count   Percent
+%    0     2192     21.92%
+%    1      343      3.43%
+%    2      362      3.62%
+%    3      801      8.01%
+%    4      362      3.62%
+%    5      698      6.98%
+%    7      337      3.37%
+%    8      352      3.52%
+%   10      713      7.13%
+%   11      322      3.22%
+%   12      715      7.15%
+%   13      320      3.20%
+%   14      327      3.27%
+%   15     2156     21.56%
+frequencies = tabulate(classifications2);
+maxfreq = max(frequencies(:,2))  % 2192
+minfreq = min(frequencies(:,2))  % 320
+verhaeltnis = maxfreq / minfreq  % 6.8500
